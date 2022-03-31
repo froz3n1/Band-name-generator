cd57555b2e7043764916cae8aeff3cf8cea33c8e
froz3n1 <76907880+froz3n1@users.noreply.github.com>
2022-03-31 02:58:07 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+print("welcome to the band name generator!!")
+print("what is your home town?")
+home_town_name=input()
+print("what is your pets name?")
+pets_name=input()
+print("your band name could be " " "+ home_town_name+pets_name )
+
+
+
